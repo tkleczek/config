a75564a144270fc873ec5b637cea1c723ef6a44f
Tomasz Kleczek <tkleczek@akamai.com>
2019-10-25 06:51:01 +0200

    Add tmux config, improve symlinks

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-ln -s $HOME/gitconfig/gitconfig $HOME/.gitconfig
+
+create_symlink() {
+  SRC_RELATIVE=$1
+  DEST=$2
+  SRC=$HOME/config/$SRC_RELATIVE
+
+  if [[ -f "$DEST" ]]; then
+    echo "File $DEST already exist. Skipping."
+  else
+    ln -s $SRC $DEST
+  fi
+}
+
+create_symlink gitconfig $HOME/.gitconfig 
+create_symlink tmux.conf $HOME/.tmux.conf 
+
